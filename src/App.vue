f998194f615f6bed487da67494aa99d584aa7627
Rodrigol <rodrigolb01@gmail.com>
2023-11-22 23:51:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+
+export default {
+  components : {
+    Header,
+    Balance
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <Header/>
+    <Balance/>
+  </div>
+</template>
---
+++ src/components/Balance.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Your Balance
+    </div>
+</template>
